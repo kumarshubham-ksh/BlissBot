d8ec0c24efb1a40cb01e4a0e41b63b037aa8fdbb
Kumar Satyam <142577338+kumarsatyam-ksa@users.noreply.github.com>
2023-10-31 19:13:50 +0530

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
